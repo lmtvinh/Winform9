2a66aceb287bd052db7c85b8ef6ce32fdf70a531
Luong Minh The Vinh <92390358+lmtvinh@users.noreply.github.com>
2023-11-10 11:16:47 +0700

    .
    .

--- WindowsFormsApp3/GUI/frmManager.Designer.cs
+++ WindowsFormsApp3/GUI/frmManager.Designer.cs
@@ -30,13 +30,20 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDocGia = new System.Windows.Forms.Button();
             this.btnQLNhanVien = new System.Windows.Forms.Button();
+            this.btnBangCap = new System.Windows.Forms.Button();
+            this.btnSach = new System.Windows.Forms.Button();
+            this.btnThuTien = new System.Windows.Forms.Button();
+            this.btnPhieuMuon = new System.Windows.Forms.Button();
+            this.panelChild = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panelChild);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -46,6 +53,11 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.btnPhieuMuon);
+            this.panel2.Controls.Add(this.btnThuTien);
+            this.panel2.Controls.Add(this.btnSach);
+            this.panel2.Controls.Add(this.btnBangCap);
+            this.panel2.Controls.Add(this.btnDocGia);
             this.panel2.Controls.Add(this.btnQLNhanVien);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
@@ -53,14 +65,73 @@
             this.panel2.Size = new System.Drawing.Size(150, 588);
             this.panel2.TabIndex = 0;
             // 
+            // btnDocGia
+            // 
+            this.btnDocGia.Location = new System.Drawing.Point(0, 128);
+            this.btnDocGia.Name = "btnDocGia";
+            this.btnDocGia.Size = new System.Drawing.Size(146, 52);
+            this.btnDocGia.TabIndex = 1;
+            this.btnDocGia.Text = "Độc Giả";
+            this.btnDocGia.UseVisualStyleBackColor = true;
+            this.btnDocGia.Click += new System.EventHandler(this.btnDocGia_Click);
+            // 
             // btnQLNhanVien
             // 
-            this.btnQLNhanVien.Location = new System.Drawing.Point(1, 3);
+            this.btnQLNhanVien.Location = new System.Drawing.Point(0, 70);
             this.btnQLNhanVien.Name = "btnQLNhanVien";
             this.btnQLNhanVien.Size = new System.Drawing.Size(146, 52);
             this.btnQLNhanVien.TabIndex = 0;
             this.btnQLNhanVien.Text = "Nhân Viên";
             this.btnQLNhanVien.UseVisualStyleBackColor = true;
+            this.btnQLNhanVien.Click += new System.EventHandler(this.btnQLNhanVien_Click);
+            // 
+            // btnBangCap
+            // 
+            this.btnBangCap.Location = new System.Drawing.Point(0, 186);
+            this.btnBangCap.Name = "btnBangCap";
+            this.btnBangCap.Size = new System.Drawing.Size(146, 52);
+            this.btnBangCap.TabIndex = 2;
+            this.btnBangCap.Text = "Bằng Cấp";
+            this.btnBangCap.UseVisualStyleBackColor = true;
+            this.btnBangCap.Click += new System.EventHandler(this.btnBangCap_Click);
+            // 
+            // btnSach
+            // 
+            this.btnSach.Location = new System.Drawing.Point(1, 12);
+            this.btnSach.Name = "btnSach";
+            this.btnSach.Size = new System.Drawing.Size(146, 52);
+            this.btnSach.TabIndex = 3;
+            this.btnSach.Text = "Sách";
+            this.btnSach.UseVisualStyleBackColor = true;
+            this.btnSach.Click += new System.EventHandler(this.btnSach_Click);
+            // 
+            // btnThuTien
+            // 
+            this.btnThuTien.Location = new System.Drawing.Point(0, 244);
+            this.btnThuTien.Name = "btnThuTien";
+            this.btnThuTien.Size = new System.Drawing.Size(146, 52);
+            this.btnThuTien.TabIndex = 4;
+            this.btnThuTien.Text = "Thu Tiền";
+            this.btnThuTien.UseVisualStyleBackColor = true;
+            this.btnThuTien.Click += new System.EventHandler(this.btnThuTien_Click);
+            // 
+            // btnPhieuMuon
+            // 
+            this.btnPhieuMuon.Location = new System.Drawing.Point(0, 302);
+            this.btnPhieuMuon.Name = "btnPhieuMuon";
+            this.btnPhieuMuon.Size = new System.Drawing.Size(146, 52);
+            this.btnPhieuMuon.TabIndex = 3;
+            this.btnPhieuMuon.Text = "Mượn Sách";
+            this.btnPhieuMuon.UseVisualStyleBackColor = true;
+            this.btnPhieuMuon.Click += new System.EventHandler(this.btnPhieuMuon_Click);
+            // 
+            // panelChild
+            // 
+            this.panelChild.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChild.Location = new System.Drawing.Point(150, 0);
+            this.panelChild.Name = "panelChild";
+            this.panelChild.Size = new System.Drawing.Size(783, 588);
+            this.panelChild.TabIndex = 1;
             // 
             // frmManager
             // 
@@ -69,7 +140,7 @@
             this.ClientSize = new System.Drawing.Size(933, 588);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmManager";
             this.Text = "frmManager";
             this.panel1.ResumeLayout(false);
@@ -83,5 +154,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnQLNhanVien;
+        private System.Windows.Forms.Button btnDocGia;
+        private System.Windows.Forms.Button btnBangCap;
+        private System.Windows.Forms.Button btnSach;
+        private System.Windows.Forms.Button btnThuTien;
+        private System.Windows.Forms.Button btnPhieuMuon;
+        private System.Windows.Forms.Panel panelChild;
     }
 }
--- WindowsFormsApp3/GUI/frmManager.cs
+++ WindowsFormsApp3/GUI/frmManager.cs
@@ -12,9 +12,56 @@ namespace WindowsFormsApp3.GUI
 {
     public partial class frmManager : Form
     {
+        private Form activeForm = null;
         public frmManager()
         {
             InitializeComponent();
+        }
+
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChild.Controls.Add(childForm);
+            panelChild.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btnSach_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmSach());
+        }
+
+        private void btnQLNhanVien_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmNhanVien());
+        }
+
+        private void btnDocGia_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmDocGia());
+        }
+
+        private void btnBangCap_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmBangCap());
+        }
+
+        private void btnThuTien_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmThuTien());
+        }
+
+        private void btnPhieuMuon_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmPhieuMuonSach());
         }
     }
 }
