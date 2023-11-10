38c341ccc9b37ef8f9897a2a30674aaccf562f4a
Luong Minh The Vinh <92390358+lmtvinh@users.noreply.github.com>
2023-11-10 09:56:56 +0700

    .
    .

---
+++ WindowsFormsApp3/DAL/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.DAL
+{
+    public class Database
+    {
+        SqlConnection sqlConn;
+        SqlDataAdapter da;
+        DataSet ds;
+        public Database()
+        {
+            string strCnn = "Data Source= localhost; Database=QLThuVien;Integrated Security = True";
+            sqlConn = new SqlConnection(strCnn);
+        }
+
+        public DataTable Execute(string sqlStr)
+        {
+            da = new SqlDataAdapter(sqlStr, sqlConn);
+            ds = new DataSet();
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        public void ExecuteNonQuery(string strSQL)
+        {
+            SqlCommand sqlcmd = new SqlCommand(strSQL, sqlConn);
+            sqlConn.Open();
+            sqlcmd.ExecuteNonQuery();
+            sqlConn.Close();
+        }
+    }
+}
